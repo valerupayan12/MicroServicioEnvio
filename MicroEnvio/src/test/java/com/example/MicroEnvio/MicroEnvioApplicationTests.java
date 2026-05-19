ae8d9e1696234b28bba8ddd14054e95fbf0b10c7
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:27:33 -0400

    Add files via upload

---
+++ MicroEnvio/src/test/java/com/example/MicroEnvio/MicroEnvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroEnvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEnvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
